1ade43080afd1fdb4ab2f869b46483a85d34543a
lucyefranco <lucyefranco@gmail.com>
2021-03-09 12:43:30 -0800

    removing sass files and moving styling to css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Routes from './config/Routes'
 import './App.css'
-import './App.scss'
 import UserModel from './models/user'
 
 function App() {
